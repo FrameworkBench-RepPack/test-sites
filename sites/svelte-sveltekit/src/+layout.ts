384ad261885d5f7f25f684a8af2c008d4b0d6fd8
Anton <dev@atjn.dk>
2025-10-22 15:24:37 +0200

    Initial setup with sveltekit

---
+++ sites/svelte-sveltekit/src/+layout.ts
@@ -0,0 +1,5 @@
+// Ensuring that all optimisations are enabled
+// Following the guide: https://svelte.dev/docs/kit/adapter-static
+export const csr = true;
+export const ssr = true;
+export const prerender = true;
