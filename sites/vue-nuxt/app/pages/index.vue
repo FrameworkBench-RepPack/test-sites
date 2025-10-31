1f4ab7b5d956d7c92a8aa54d1df43a22744027ed
Anton <dev@atjn.dk>
2025-10-31 18:08:10 +0100

    Fix invalid homepage syntax

--- sites/vue-nuxt/app/pages/index.vue
+++ sites/vue-nuxt/app/pages/index.vue
@@ -19,7 +19,7 @@ definePageMeta({
         <LinksComponent />
       </ul>
     </header>
-    <section class="page-width">
+    <div class="page-width">
       <p>
         Pellentesque finibus urna at ligula aliquet finibus. Ut rutrum dolor
         vitae quam molestie, at dapibus enim blandit. Maecenas sit amet blandit
@@ -328,7 +328,7 @@ definePageMeta({
         sagittis gravida ligula. Quisque facilisis lorem leo, at mattis metus
         vulputate quis.
       </p>
-    </section>
+    </div>
   </main>
 </template>
 
@@ -350,7 +350,7 @@ h1 > a {
   display: flex;
   gap: 2em;
   justify-content: center;
-  padding: none;
+  padding: 0;
   list-style-type: none;
   font-size: 1.3em;
 }
--- sites/vue-vite/src/paths/HomePage.vue
+++ sites/vue-vite/src/paths/HomePage.vue
@@ -15,7 +15,7 @@ import ListComponent from "../components/ListComponent.vue";
         <LinksComponent />
       </ul>
     </header>
-    <section class="page-width">
+    <div class="page-width">
       <p>
         Pellentesque finibus urna at ligula aliquet finibus. Ut rutrum dolor
         vitae quam molestie, at dapibus enim blandit. Maecenas sit amet blandit
@@ -324,7 +324,7 @@ import ListComponent from "../components/ListComponent.vue";
         sagittis gravida ligula. Quisque facilisis lorem leo, at mattis metus
         vulputate quis.
       </p>
-    </section>
+    </div>
   </main>
 </template>
 
@@ -346,7 +346,7 @@ h1 > a {
   display: flex;
   gap: 2em;
   justify-content: center;
-  padding: none;
+  padding: 0;
   list-style-type: none;
   font-size: 1.3em;
 }
