91b72a3363f315248e2fb267f5353ab0aaf734d4
Anton <dev@atjn.dk>
2025-11-14 15:42:49 +0100

    angular-analog: better removal in postprocessing step

--- sites/angular-analog/bin/postprocess.ts
+++ sites/angular-analog/bin/postprocess.ts
@@ -4,13 +4,14 @@ import fs from "fs-extra";
 import path from "path";
 
 const distDir = path.join(import.meta.dirname, "..", "dist");
-const publicDir = path.join(distDir, "analog", "public");
+const analogDir = path.join(distDir, "analog");
+const publicDir = path.join(analogDir, "public");
 
 console.log("Running postprocess steps to make file structure conformant..");
 
 if (await fs.pathExists(publicDir)) {
   await fs.copy(publicDir, distDir, { overwrite: true });
-  await fs.remove(publicDir);
+  await fs.remove(analogDir);
 } else {
   throw new Error(
     "❌ No 'analog/public' folder found, maybe the postprocessing step has already been executed?",
