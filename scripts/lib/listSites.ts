1a0bfaf5ebe34e84ff8e248d5756d80ac1b2e60e
Anton <dev@atjn.dk>
2025-11-14 08:35:25 +0100

    Handle text files in sites dir

--- scripts/lib/listSites.ts
+++ scripts/lib/listSites.ts
@@ -5,9 +5,15 @@ import { sitesFolder } from "./dirs.ts";
 let sites: string[] | undefined;
 
 export async function listSites(): Promise<string[]> {
-  sites ??= (await fs.readdir(sitesFolder)).filter(async (name) => {
-    const fullPath = join(sitesFolder, name);
-    return (await fs.stat(fullPath)).isDirectory();
-  });
+  if (!sites) {
+    const entries = await fs.readdir(sitesFolder);
+    const dirTests = await Promise.all(
+      entries.map(async (name) => {
+        const fullPath = join(sitesFolder, name);
+        return (await fs.stat(fullPath)).isDirectory();
+      }),
+    );
+    sites = entries.filter((_, index: number) => dirTests[index]);
+  }
   return sites;
 }
