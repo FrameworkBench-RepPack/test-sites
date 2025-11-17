1125d293575a1139fc2fe9edb719b2b1dea2a68c
Bastian Hansen <bastian.b.hansen@gmail.com>
2025-11-17 16:12:05 +0100

    Fix footer location

--- sites/react-nextjs/src/app/layout.tsx
+++ sites/react-nextjs/src/app/layout.tsx
@@ -14,9 +14,11 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          {children}
+          <Footer />
+        </body>
       </html>
-      <Footer />
     </>
   );
 }
