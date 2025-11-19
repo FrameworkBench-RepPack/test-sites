11f632477664e7c61a29ef803493499d26587d44
Bastian Hansen <bastian.b.hansen@gmail.com>
2025-11-19 14:35:05 +0100

    Avoid using tsPaths vite plugin

--- sites/react-react-router-declarative/vite.config.ts
+++ sites/react-react-router-declarative/vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
--- sites/react-react-router-framework/vite.config.ts
+++ sites/react-react-router-framework/vite.config.ts
@@ -1,7 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "app"),
+    },
+  },
 });
