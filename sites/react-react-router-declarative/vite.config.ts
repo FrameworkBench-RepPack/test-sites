35647d0fdd91c7b7f7fbbf35c0346cb5c1d08991
Bastian Hansen <bastian.b.hansen@gmail.com>
2025-11-19 14:19:09 +0100

    Improve vite config

--- sites/react-react-router-declarative/vite.config.ts
+++ sites/react-react-router-declarative/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,10 +10,6 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tsconfigPaths(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
 });
