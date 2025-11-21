75e802314714ce1196ec9853676bd6d97cc2f3e0
Bastian Hansen <bastian.b.hansen@gmail.com>
2025-11-21 08:20:45 +0100

    Fix live

--- sites/preact-vite/src/components/live/Live.tsx
+++ sites/preact-vite/src/components/live/Live.tsx
@@ -1,13 +1,14 @@
 import liveData from "../../assets/liveData.json" with { type: "json" };
-import { computed, useSignal } from "@preact/signals";
+import { useSignal } from "@preact/signals";
 import s from "./Live.module.css";
+import { useEffect } from "preact/hooks";
 
 export default function Live() {
   const INTERVAL = 1000 as const;
 
   const index = useSignal(0);
 
-  computed(() => {
+  useEffect(() => {
     let timeoutId: number | NodeJS.Timeout;
     function stepData() {
       index.value = (index.value + 1) % (liveData.length - 1);
@@ -17,7 +18,7 @@ export default function Live() {
     return () => {
       clearTimeout(timeoutId);
     };
-  });
+  }, []);
 
   return (
     <table id="live-data" className={s.liveData}>
