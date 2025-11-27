7e1892fb64e8adca0f9059fd03a8569eeb3a9054
Anton <dev@atjn.dk>
2025-11-27 14:52:11 +0100

    Switch to js-based details in vanilla sites

---
+++ sites/vanilla/public/assets/components/details.js
@@ -0,0 +1,6 @@
+for (const details of document.querySelectorAll(".details")) {
+  const button = details.querySelector(".summary");
+  button.addEventListener("click", () => {
+    details.classList.toggle("open");
+  });
+}
