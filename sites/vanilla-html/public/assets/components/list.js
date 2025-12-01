a5688c8b74d7c50e8ced89feff083926532453ed
Anton <dev@atjn.dk>
2025-12-01 15:50:11 +0100

    Fix wrong rename

--- sites/vanilla-html/public/assets/components/list.js
+++ sites/vanilla-html/public/assets/components/list.js
@@ -2,11 +2,7 @@ const MAX_SLACK_MS = 700;
 
 const form = document.querySelector("#list > .controls > form");
 const tbody = document.querySelector("#list > .data tbody");
-const table = tbody.parentElement;
-
-const noDataMessage = document.createElement("p");
-noDataMessage.classList.add("no-data-message");
-noDataMessage.innerText = "No entries matched the filter settings.";
+const noDataMessage = document.querySelector("#list > .data .no-data-message");
 
 const rows = [...tbody.querySelectorAll("tr")].map((rowElement) => {
   const cells = rowElement.querySelectorAll("td");
@@ -40,38 +36,31 @@ function filterList() {
   const ageTo = Number(data.get("age-to"));
   const categories = data.getAll("category").map((entry) => Number(entry));
   let noData = true;
-  const sortedRows = rows.sort((a, b) => {
-    switch (sortOption) {
-      case "name": {
-        return a.name.localeCompare(b.name);
+  rows
+    .sort((a, b) => {
+      switch (sortOption) {
+        case "name": {
+          return a.name.localeCompare(b.name);
+        }
+        case "age": {
+          return a.age - b.age;
+        }
+        case "category": {
+          return a.category - b.category;
+        }
+        default: {
+          throw TypeError("Unknown option");
+        }
       }
-      case "age": {
-        return a.age - b.age;
-      }
-      case "category": {
-        return a.category - b.category;
-      }
-      default: {
-        throw TypeError("Unknown option");
-      }
-    }
-  });
-  tbody.remove();
-  for (const row of sortedRows) {
-    const hidden =
-      row.age < ageFrom ||
-      row.age > ageTo ||
-      !categories.includes(row.category);
-    row.element.remove();
-    if (!hidden) {
-      tbody.appendChild(row.element);
-      noData = false;
-    }
-  }
-  noDataMessage.remove();
-  if (noData) {
-    table.insertAdjacentElement("afterend", noDataMessage);
-  } else {
-    table.appendChild(tbody);
-  }
+    })
+    .forEach((row) => {
+      tbody.appendChild(tbody.removeChild(row.element));
+      const hidden =
+        row.age < ageFrom ||
+        row.age > ageTo ||
+        !categories.includes(row.category);
+      row.element.hidden = hidden;
+      if (!hidden) noData = false;
+    });
+  noDataMessage.hidden = !noData;
 }
--- sites/vanilla-js/public/assets/components/list.js
+++ sites/vanilla-js/public/assets/components/list.js
@@ -2,7 +2,11 @@ const MAX_SLACK_MS = 700;
 
 const form = document.querySelector("#list > .controls > form");
 const tbody = document.querySelector("#list > .data tbody");
-const noDataMessage = document.querySelector("#list > .data .no-data-message");
+const table = tbody.parentElement;
+
+const noDataMessage = document.createElement("p");
+noDataMessage.classList.add("no-data-message");
+noDataMessage.innerText = "No entries matched the filter settings.";
 
 const rows = [...tbody.querySelectorAll("tr")].map((rowElement) => {
   const cells = rowElement.querySelectorAll("td");
@@ -36,31 +40,38 @@ function filterList() {
   const ageTo = Number(data.get("age-to"));
   const categories = data.getAll("category").map((entry) => Number(entry));
   let noData = true;
-  rows
-    .sort((a, b) => {
-      switch (sortOption) {
-        case "name": {
-          return a.name.localeCompare(b.name);
-        }
-        case "age": {
-          return a.age - b.age;
-        }
-        case "category": {
-          return a.category - b.category;
-        }
-        default: {
-          throw TypeError("Unknown option");
-        }
+  const sortedRows = rows.sort((a, b) => {
+    switch (sortOption) {
+      case "name": {
+        return a.name.localeCompare(b.name);
       }
-    })
-    .forEach((row) => {
-      tbody.appendChild(tbody.removeChild(row.element));
-      const hidden =
-        row.age < ageFrom ||
-        row.age > ageTo ||
-        !categories.includes(row.category);
-      row.element.hidden = hidden;
-      if (!hidden) noData = false;
-    });
-  noDataMessage.hidden = !noData;
+      case "age": {
+        return a.age - b.age;
+      }
+      case "category": {
+        return a.category - b.category;
+      }
+      default: {
+        throw TypeError("Unknown option");
+      }
+    }
+  });
+  tbody.remove();
+  for (const row of sortedRows) {
+    const hidden =
+      row.age < ageFrom ||
+      row.age > ageTo ||
+      !categories.includes(row.category);
+    row.element.remove();
+    if (!hidden) {
+      tbody.appendChild(row.element);
+      noData = false;
+    }
+  }
+  noDataMessage.remove();
+  if (noData) {
+    table.insertAdjacentElement("afterend", noDataMessage);
+  } else {
+    table.appendChild(tbody);
+  }
 }
