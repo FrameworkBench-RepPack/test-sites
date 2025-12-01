c0150b2cb72b1d5ca43536b6d145baaf6edb86ab
Anton <dev@atjn.dk>
2025-12-01 10:16:36 +0100

    Optimize details in vanilla-vite

--- sites/vanilla-vite-optimized/src/components/details.ts
+++ sites/vanilla-vite-optimized/src/components/details.ts
@@ -1,6 +1,13 @@
-for (const details of document.querySelectorAll(".details")) {
-  const button = details.querySelector(".summary")!;
-  button.addEventListener("click", () => {
-    details.classList.toggle("open");
-  });
-}
+document.body.addEventListener("click", (event: Event) => {
+  const element = (event.target as HTMLElement)?.parentElement;
+  if (!element?.classList?.contains("details")) return;
+
+  if (element.classList.toggle("open")) {
+    const contents = document.createElement("div");
+    contents.classList.add("contents");
+    contents.innerHTML = `<p>${element.dataset.contents}</p>`;
+    element.appendChild(contents);
+  } else {
+    element.querySelector(".contents")?.remove();
+  }
+});
