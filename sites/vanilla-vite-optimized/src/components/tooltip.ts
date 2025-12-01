03fdf5ddfd6a107bc8b21c749d677f6211f59c2f
Anton <dev@atjn.dk>
2025-12-01 11:24:39 +0100

    Optimize tooltips in vanilla-vite

---
+++ sites/vanilla-vite-optimized/src/components/tooltip.ts
@@ -0,0 +1,23 @@
+const contents = document.createElement("span");
+contents.classList.add("contents");
+let tooltipOpen: HTMLElement | undefined;
+
+document.body.addEventListener("click", (event: Event) => {
+  const element = event.target as HTMLElement;
+  if (tooltipOpen) {
+    if (
+      element !== tooltipOpen &&
+      element !== contents &&
+      !contents.contains(element)
+    ) {
+      contents.remove();
+      tooltipOpen = undefined;
+    }
+  } else {
+    if (element?.classList?.contains("tooltip")) {
+      contents.innerText = element.dataset.contents!;
+      element.insertAdjacentElement("afterend", contents);
+      tooltipOpen = element;
+    }
+  }
+});
